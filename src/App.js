57583c2eb5e8c765a2c67b4ba21dce42fbe3d292
davisstacked <brettzvi@gmail.com>
2021-09-18 12:55:46 -0700

    Pokedex component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Pokedex from "./Pokedex";
-import Pokecard from "./Pokecard";
 
 function App() {
   return (
     <div className="App">
-      {/* <Pokedex /> */}
-      <Pokecard name="Charmander" id={4} type="fire"  base_experience={62} />
+      <Pokedex />
+      {/* <Pokecard name="Charmander" id={4} type="fire"  base_experience={62} /> */}
     </div>
   );
 }
